c91b33e30a7f5075a651ce60d47cccc37d17fb6c
Jacob Mischka <jacobmischka@gmail.com>
2017-03-10 10:35:32 -0600

    Add script to batch remake item banks

--- remake_item_banks.py
+++ remake_item_banks.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from os import scandir, path
 from argparse import ArgumentParser
+from os import scandir, path
 import sys
 
-def get_switch(item_bank_path):
-	bank_dir = path.realpath(item_bank_path)
+from to_item_bank import get_bank_file
 
-	switch = ''
+def remake_banks(item_bank_path):
+	bank_dir = path.realpath(item_bank_path)
 
 	with scandir(bank_dir) as bank_files:
 		for bank_file in bank_files:
@@ -15,28 +15,26 @@ def get_switch(item_bank_path):
 				bank_class_name = path.splitext(path.basename(bank_file))[0]
 				with open(bank_file, 'r') as bank_file_contents:
 					for line in bank_file_contents:
-						if '* Name: ' in line:
-							measure_name = line.replace('* Name: ', '').strip()
+						if '* OID: ' in line:
+							oid = line.replace('* OID: ', '').strip()
 							break
-				if not measure_name:
+				if not oid:
 					raise LookupError('Error finding measure name for {}'.format(bank_class_name))
 
-				switch += 'case "{}": return {}.bank();\n'.format(
-					measure_name.replace('"', '\\"'),
-					bank_class_name
-				)
+				item_bank = get_bank_file(oid, bank_class_name) + '\n'
+				with open(bank_file, 'w') as bank_fp:
+					bank_fp.write(item_bank)
 			except Exception as e:
-				print(e, file=sys.stderr)
-
-	return switch
+				print('Error rewriting bank: {}'.format(e), file=sys.stderr)
 
 def main():
-	parser = ArgumentParser(description='Create Java switch statement for PROMIS measures in given directory')
+	parser = ArgumentParser(description='Mass recreate item banks')
 	parser.add_argument('directory', action='store', help='Directory in which to look for Java PROMIS item banks')
 
 	args = parser.parse_args()
 
-	print(get_switch(args.directory))
+	remake_banks(args.directory)
+
 
 if __name__ == '__main__':
 	main()
